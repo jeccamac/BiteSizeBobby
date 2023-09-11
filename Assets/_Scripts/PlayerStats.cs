dcc8e17afe21af5a09332c9d139945ce594a54f3
Jessica Macaspac <jeccamac@gmail.com>
2023-09-11 15:17:20 -0500

    Health UI Animation
    Updated the Health UI with heart beat animation and empty slots when losing lives.

--- Assets/_Scripts/PlayerStats.cs
+++ Assets/_Scripts/PlayerStats.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] int _health = 3;
     [SerializeField] int _maxHealth = 3;
-    public GameObject _healthIcon1;
-    public GameObject _healthIcon2;
-    public GameObject _healthIcon3;
+    public ImageAnimation _healthIcon1;
+    public ImageAnimation _healthIcon1_empty;
+    public ImageAnimation _healthIcon2;
+    public ImageAnimation _healthIcon2_empty;
+    public ImageAnimation _healthIcon3;
+    public ImageAnimation _healthIcon3_empty;
 
-    private void Awake()
-    {
-        //get heart icons
-    }
     public void Update() 
     {
         UpdateHealth();
@@ -29,32 +30,44 @@ public class PlayerStats : MonoBehaviour
             case 0:
                 {
                     _healthIcon1.gameObject.SetActive(false);
+                    _healthIcon1_empty.gameObject.SetActive(true);
                     _healthIcon2.gameObject.SetActive(false);
+                    _healthIcon2_empty.gameObject.SetActive(true);
                     _healthIcon3.gameObject.SetActive(false);
+                    _healthIcon3_empty.gameObject.SetActive(true);
                     break;
                 }
 
             case 1:
                 {
                     _healthIcon1.gameObject.SetActive(true);
+                    _healthIcon1_empty.gameObject.SetActive(false);
                     _healthIcon2.gameObject.SetActive(false);
+                    _healthIcon2_empty.gameObject.SetActive(true);
                     _healthIcon3.gameObject.SetActive(false);
+                    _healthIcon3_empty.gameObject.SetActive(true);
                     break;
                 }
 
             case 2:
                 {
                     _healthIcon1.gameObject.SetActive(true);
+                    _healthIcon1_empty.gameObject.SetActive(false);
                     _healthIcon2.gameObject.SetActive(true);
+                    _healthIcon2_empty.gameObject.SetActive(false);
                     _healthIcon3.gameObject.SetActive(false);
+                    _healthIcon3_empty.gameObject.SetActive(true);
                     break;
                 }
             
             case 3:
                 {
                     _healthIcon1.gameObject.SetActive(true);
+                    _healthIcon1_empty.gameObject.SetActive(false);
                     _healthIcon2.gameObject.SetActive(true);
+                    _healthIcon2_empty.gameObject.SetActive(false);
                     _healthIcon3.gameObject.SetActive(true);
+                    _healthIcon3_empty.gameObject.SetActive(false);
                     break;
                 }
         }
